7dc1ce84e0216a91d7ad88ea16e4cd52250817f1
nancy nguyen <prancynguyen@gmail.com>
2017-12-08 11:18:42 -0800

    Add React-Router-Dom and Create App Pages

---
+++ src/pages/App/App.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import {BrowserRouter as Router,
+  Link,
+  Route,
+  Switch
+} from 'react-router-dom';
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          Hello
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
